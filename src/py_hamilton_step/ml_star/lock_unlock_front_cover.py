37e7af779edae91d7f1e4ce471c0a867cba136b6
Bradley Bare <bradleybare@gmail.com>
2026-04-09 21:33:31 -0400

    Implemented and tested on Hamilton

--- src/py_hamilton_step/ml_star/lock_unlock_front_cover.py
+++ src/py_hamilton_step/ml_star/lock_unlock_front_cover.py
@@ -5,45 +5,53 @@ from ..hamilton import (
     HamiltonCommand,
     HamiltonResponse,
     HamiltonStepReturnBlockDataPackage,
-    HamiltonStepReturnDescriptionPackage,
 )
 
 
 @dataclasses.dataclass(frozen=True)
 class LockUnlockFrontCoverResponse(HamiltonResponse):
-    raw_cover_lock_status_flag_as_block_data: str
-    cover_lock_status_flag_as_block_data: HamiltonStepReturnBlockDataPackage = dataclasses.field(init=False)
+    raw_cover_lock_status_as_block_data: str
+    cover_lock_status_as_block_data: HamiltonStepReturnBlockDataPackage = dataclasses.field(init=False)
 
-    raw_cover_lock_status_flag: str
-    cover_lock_status_flag: HamiltonStepReturnDescriptionPackage = dataclasses.field(init=False)
+    raw_cover_lock_status_flag: int
+    cover_lock_status_flag: str = dataclasses.field(init=False)
 
     def __post_init__(self):
         object.__setattr__(
             self,
-            "cover_lock_status_flag_as_block_data",
-            HamiltonStepReturnBlockDataPackage.parse_raw_step_return(self.raw_cover_lock_status_flag_as_block_data),
+            "cover_lock_status_as_block_data",
+            HamiltonStepReturnBlockDataPackage.parse_raw_step_return(self.raw_cover_lock_status_as_block_data),
         )
+
         object.__setattr__(
             self,
             "cover_lock_status_flag",
-            HamiltonStepReturnDescriptionPackage.parse_raw_step_return(self.raw_cover_lock_status_flag),
+            "unlocked" if self.raw_cover_lock_status_flag == 0 else "locked",
         )
 
 
+_front_setting_by_name = {
+    "unlocked": 0,
+    "locked": 1,
+}
+
+
 @dataclasses.dataclass(kw_only=True, frozen=True)
-class LockUnlockFrontCover(HamiltonCommand):
+class LockUnlockFrontCoverCommand(HamiltonCommand):
     front: typing.Literal["unlocked", "locked"] = "locked"
 
     def as_dict(self) -> dict:
         command_dict = super().as_dict()
 
-        command_dict["front"] = self.front
+        args = command_dict["args"]
+
+        args["front"] = _front_setting_by_name[self.front]
 
         return command_dict
 
     def parse_response(self, data: dict) -> LockUnlockFrontCoverResponse:
         return LockUnlockFrontCoverResponse(
             command_id=data["command_id"],
-            raw_cover_lock_status_flag_as_block_data=data["raw_cover_lock_status_flag_as_block_data"],
+            raw_cover_lock_status_as_block_data=data["raw_cover_lock_status_as_block_data"],
             raw_cover_lock_status_flag=data["raw_cover_lock_status_flag"],
         )
